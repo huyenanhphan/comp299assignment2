8152785c5e5eaabd6b634dfb337378c8a4d252d4
huyenanhphan <hphan26@my.centennialcollege.ca>
2022-10-25 13:32:44 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,9 @@
 module.exports =
  {
- "URI":"mongodb://127.0.0.1:27017/"
+ //"URI":"mongodb://127.0.0.1:27017/",
+ //"URI":"mongodb+srv://hanh:hAnh1808@cluster0.cemuhsj.mongodb.net/?retryWrites=true&w=majority"
+ "URI":"mongodb+srv://hanh1808:hAnh1808@cluster0.cemuhsj.mongodb.net/?retryWrites=true&w=majority"
+
+ //"URI":"mongodb+srv://hanh:hanh1808@mongodbserver.2g3vmmn.mongodb.net/?retryWrites=true&w=majority"
+ //"URI":"mongodb+srv://hanh:hAnh1808@cluster0.cemuhsj.mongodb.net/?retryWrites=true&w=majority"
 }
